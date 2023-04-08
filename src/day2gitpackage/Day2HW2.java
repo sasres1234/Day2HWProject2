d2e1720f0dbbadbe9b2b745ccd3abecfb04fa56d
wedis <wedis@10.0.0.91>
2023-04-08 14:37:53 -0400

    Initial Commit

--- src/day2gitpackage/Day2HW2.java
+++ src/day2gitpackage/Day2HW2.java
@@ -7,4 +7,18 @@ public class Day2HW2 {
 		System.out.println("Day 2 HW2");
 	}
 
+	public void division(int a, int b) {
+		
+		System.out.println(a/b);
+	}
+	
+	public void add() {
+		
+		System.out.println(2+3);
+	}
+	
+      public void add2() {
+		
+		System.out.println(2+3);
+}
 }
